af555b6b6803e94aefe6808e1994ab59bd0e63d7
Saint Wesonga <sawesong@microsoft.com>
2022-01-03 12:19:00 -0700

    Create filter for atomics benchmarks

--- java/jmh/run_jmh_jdk_micros.sh
+++ java/jmh/run_jmh_jdk_micros.sh
@@ -205,9 +205,11 @@ run_benchmarks()
     echo "**** $jvm_heap_size_opts $timestamp Benchmarks completed for filter $benchmark_filter_regex ****"
 }
 
+atomics_benchmark_filter="(?i)\.*(atomic|lock|volatile|ConcurrentHashMap|ProducerConsumer|Queues|ThreadLocalRandomNextInt)\.*"
 crypto_benchmark_filter="(?i)crypto"
 
 # Start the actual benchmark run
+run_benchmarks $atomics_benchmark_filter
 run_benchmarks $crypto_benchmark_filter
 
 # Display the date and time when the benchmarks complete
